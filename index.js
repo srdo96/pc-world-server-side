667403140ed69ca7b75abc7602303d2a810a3aba
Sakib Rahman <seu.sakib@gmail.com>
2022-04-30 02:58:13 +0600

    add / api

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const express = require("express");
+const { MongoClient } = require("mongodb");
 const cors = require("cors");
+const express = require("express");
+const { get } = require("express/lib/response");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("WelCome to Pc World Server");
+});
 
 app.listen(port, () => {
   console.log("Listening to port ->", port);
