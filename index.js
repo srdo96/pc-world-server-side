c1313d23c0fe8089da219cc4528ca835264aefe8
Sakib Rahman <seu.sakib@gmail.com>
2022-04-30 03:00:50 +0600

    connect server to mongo database

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ app.get("/", (req, res) => {
   res.send("WelCome to Pc World Server");
 });
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h2ajj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const items = client.db("pc-world").collection("item");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log("Listening to port ->", port);
 });
